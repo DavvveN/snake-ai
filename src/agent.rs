2e9cd6b65aaf1193611e59d9cf90d0ee83d70bd6
David Nilsson <65786975+DavvveN@users.noreply.github.com>
2025-04-06 22:19:51 +0200

    adds agents and brain able to decide actions

--- src/agent.rs
+++ src/agent.rs
@@ -7,8 +7,20 @@ pub struct Agent {
 }
 
 impl Agent {
-    pub fn run_game() {
+    pub fn run_game(&mut self, max_steps: u32) {
         let mut game = Game::new();
-        while game.next_step() {}
+        while !game.game_over && game.steps_survived < max_steps {
+            let state = game.state_extraction();
+            let action = self.brain.decide(&state);
+
+            match action {
+                0 => game.snake.turn_left(),
+                1 => {}
+                2 => game.snake.turn_right(),
+                _ => panic!("Invalid Action"),
+            }
+        }
+        game.next_step();
+        self.fitness = game.compute_fitness(game.steps_survived);
     }
 }
--- src/brain.rs
+++ src/brain.rs
@@ -10,4 +10,24 @@ impl Brain {
         let weigths = (0..(3 * 12)).map(|_| rng.random_range(1.0..1.0)).collect();
         Self { weights: weigths }
     }
+
+    pub fn decide(&self, state: &Vec<f32>) -> usize {
+        let action_left = Self::dot(&self.weights[0..12], &state);
+        let action_front = Self::dot(&self.weights[12..24], &state);
+        let action_right = Self::dot(&self.weights[24..36], &state);
+
+        let actions = [action_left, action_front, action_right];
+
+        let max_action = actions
+            .iter()
+            .enumerate()
+            .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+            .unwrap()
+            .0;
+        max_action
+    }
+
+    fn dot(v1: &[f32], v2: &[f32]) -> f32 {
+        v1.iter().zip(v2.iter()).map(|(a, b)| a * b).sum()
+    }
 }
